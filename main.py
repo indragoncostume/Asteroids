0b3609850dd067827061023efd3099c09d80f3c2
indragoncostume <ahmdnrwhd@gmail.com>
2025-08-25 12:53:10 +0700

    Initial Commit: Make a new repository

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from  constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    BLACK = (0, 0, 0)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill(BLACK)
+        pygame.display.flip()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
